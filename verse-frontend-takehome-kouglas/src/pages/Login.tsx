f6dd13f45742b76f34bfecdb990aa82a65513015
Kari <kouglas2@gmail.com>
2023-04-20 19:07:42 -0500

    update index.tsx
    styled login button
    add href that leads to auth0 login

--- verse-frontend-takehome-kouglas/src/pages/Login.tsx
+++ verse-frontend-takehome-kouglas/src/pages/Login.tsx
@@ -5,22 +5,22 @@ const Login = () => {
                 <form action="">
                   <div className="grid gap-9 mb-6 lg:grid-cols-2">
                     <div className="relative">
-                      <input type="text" id="floating_outlined" className="block px-2.5 pb-2.5 pt-7 w-64 text-sm text-gray-900 bg-transparent rounded-lg border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Enter your username or email " />
+                      <input type="text" id="floating_outlined" className="block px-2.5 pb-2.5 pt-7 w-64 text-sm text-gray-900 bg-transparent rounded-md border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Enter your username or email " />
                       <label htmlFor="floating_outlined" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 pb-8 z-10 origin-[0] dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-0 peer-focus:-translate-y-6 left-1 font-extrabold">Username</label>
                     </div>
                     <div className="relative">
-                      <input type="text" id="floating_outlined" className="block px-2.5 pb-2.5 pt-7 w-64 text-sm text-gray-900 bg-transparent rounded-lg border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Enter your password " />
+                      <input type="text" id="floating_outlined" className="block px-2.5 pb-2.5 pt-7 w-64 text-sm text-gray-900 bg-transparent rounded-md border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Enter your password " />
                       <label htmlFor="floating_outlined" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 pb-8 z-10 origin-[0] dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-0 peer-focus:-translate-y-6 left-1 font-extrabold">Password</label>
                     </div>
                     </div>
                   
-                    <div className=" border-cyan-500 border-2 float-left">
+                    <div className="float-left mb-3">
                     <input 
                       type="checkbox"
                       name="checkbox" 
                       title="checkbox"
                     />
-                    <label className="  mt-0 text-sm" htmlFor="checkbox"> Remember me</label>
+                    <label className=" text-sm" htmlFor="checkbox"> Remember me</label>
                     </div>
                     
                 
--- verse-frontend-takehome-kouglas/src/pages/index.tsx
+++ verse-frontend-takehome-kouglas/src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <h1 className="fixed left-0 top-0 flex w-full border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit font-extrabold tracking-widest pl-8">
           <Link href="https://verse.inc" >Verse</Link>
       <div className="border-1 border-black absolute right-8 p-3 bottom-3 bg-black  text-white font-light rounded-md">
-        <Link href="#" className="">
+        <Link href="/api/auth/login" className="">
           Sign up
         </Link>
       </div>
@@ -23,7 +23,7 @@ export default function Home() {
       <div className="grid text-center ">
         <a
           href="https://verse.inc"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 mb-80"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 mb-8"
           target="_blank"
           rel="noopener noreferrer"
         >    
@@ -34,15 +34,14 @@ export default function Home() {
             alt="verse logo"
             className="ml-auto mr-auto"
           />
-          put auth here 
         </a>
         <Login />
+        <a 
+          href="/api/auth/login"
+          className='border-1 border-black w-11/12 bg-black text-white text-sm px-2.5 pb-3 ml-auto mr-auto pt-3 rounded-md mt-2'
+          >Log In</a>  
 
-
-        <a href="/api/auth/login">Login</a>  
-
-        {/* logout */}
-        <a href="/api/auth/logout">Logout</a>
+        {/* <a href="/api/auth/logout">Logout</a> */}
         
       </div>
     </main>
