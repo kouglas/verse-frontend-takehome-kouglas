8abe6ea2fb779db322653ce6e3f0053af803473c
Kari <kouglas2@gmail.com>
2023-04-23 12:37:44 -0500

    update Cards.tsx
    Add Image component instead of <img/> element. mostly for better performance

--- verse-frontend-takehome-kouglas/src/pages/components/Cards.tsx
+++ verse-frontend-takehome-kouglas/src/pages/components/Cards.tsx
@@ -1,4 +1,5 @@
 import { CardsInfo } from "../interfaces/CardInfo";
+import Image from "next/image";
 
 const cardsInfo: CardsInfo[] = [
   {
@@ -29,14 +30,20 @@ const cardsInfo: CardsInfo[] = [
 
 const Cards = () => {
   return ( 
-    <div className="grid gap-4 grid-cols-2 grid-rows-2">
+    <div className="grid gap-2 grid-cols-2 grid-rows-2">
       {cardsInfo.map((card) => (
         <a 
         href="card.buttonLink"
         key={card.title}
         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
-          <img src={card.image} className="w-64 h-64 object-cover mb-3" />
+          <Image
+            src="/Verse-Logo.png"
+            height={144}
+            width={144}
+            alt="verse logo"
+            className="ml-auto mr-auto"
+          />
           <h5 className="mb-3 text-2xl font-semibold">{card.title}</h5>
           <button>Sign up</button>
         </a>
