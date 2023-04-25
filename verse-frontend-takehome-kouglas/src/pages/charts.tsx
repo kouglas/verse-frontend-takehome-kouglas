849b65d3df41b291754637d2d83fe6bed3f4d25a
Kari <kouglas2@gmail.com>
2023-04-25 18:02:53 -0500

    update charts.tsx
    create charts route
    next test app with jest
    implement chart.js

--- verse-frontend-takehome-kouglas/src/pages/charts.tsx
+++ verse-frontend-takehome-kouglas/src/pages/charts.tsx
@@ -1,14 +1,8 @@
-import React, { use } from 'react';
-import { useUser } from '@auth0/nextjs-auth0/client';
-import { useAuth0 } from '@auth0/auth0-react';
-import Link from 'next/link';
-import SideNav from './components/SideNav';
-import Cards from './components/Cards';
+import Link from "next/link";
 
-export default function Profile() {
-
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 pb-4 pt-20">
+const Charts = () => {
+  return ( 
+    <section>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm">
         <h1 className="fixed left-0 top-0 flex w-full border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit font-extrabold tracking-widest pl-8" >
           <Link href="https://verse.inc" >Verse</Link>
@@ -17,17 +11,12 @@ export default function Profile() {
               {/* the above link needs to  be an icon and not a button w/ text */}
             </div>
         </h1> 
-        <SideNav />
       </div>
-      <h6>Select the card that best fits the data you want to see</h6>
-      <section className="border-2 border-red-500">
-        <Cards />
-      </section>
-  
+    </section>
 
+    
 
-
-
-    </main>
   );
 }
+
+export default Charts;
--- verse-frontend-takehome-kouglas/src/pages/components/Cards.tsx
+++ verse-frontend-takehome-kouglas/src/pages/components/Cards.tsx
@@ -1,5 +1,6 @@
 import { CardsInfo } from "../interfaces/CardInfo";
 import Image from "next/image";
+import Link from "next/link";
 
 const cardsInfo: CardsInfo[] = [
   {
@@ -33,7 +34,7 @@ const Cards = () => {
     
     <div className="grid gap-2 grid-cols-2 grid-rows-2 mb-10">
       {cardsInfo.map((card) => (
-        <div key={card.title} className="group rounded-lg border border-transparent w-64 h-72 px-5 py-4 transition-colors bg-gray-100 hover:border-gray-300 hover:bg-blue-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+        <div key={card.title} className="rounded-lg border border-transparent w-64 h-72 px-5 py-4 transition-colors bg-gray-100 hover:border-gray-300 hover:bg-blue-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
           <Image
             src="/Verse-Logo.png"
             height={96}
@@ -41,9 +42,9 @@ const Cards = () => {
             alt="verse logo"
             className="ml-auto mr-auto"
           />
-          <p className="mb-3 text-center mt-4">{card.title}</p>
-          <div className="border-2 border-blue-500 text-center justify-center">
-          <a href="">Set up</a>
+          <p className="mb-3 text-center mt-8 truncate p-2">{card.title}</p>
+          <div className=" text-center justify-center">
+          <Link href="/charts" className="py-2 px-8 rounded-full border-1  no-underline bg-gray-200">Set up</Link>
           </div>
         </div>
       
